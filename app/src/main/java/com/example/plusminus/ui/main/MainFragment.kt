7f97db43a5d86f018326dc2bb40fcf1b3408fc99
前田正博 <maedamasahiro@Mac-mini.local>
2024-04-01 19:02:18 +0900

    [update] 足し算のロジックを変更

--- app/src/main/java/com/example/plusminus/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/plusminus/ui/main/MainFragment.kt
@@ -101,13 +101,18 @@ class MainFragment : Fragment() {
      */
     private fun createPlusQuestions(createCount: Int): List<Question> {
         return (0 until createCount).map {
-            val value1 = Random.nextInt(1, 10)
-            val value2 = Random.nextInt((10 - value1), 10)
+            val value1 = Random.nextInt(1, 100)
+            val value1Str = value1.toString()
+            val onePlace = value1Str.substring(value1Str.length - 1, value1Str.length).toInt()
+            val value2 = if (onePlace == 0) {
+                10
+            } else {
+                Random.nextInt((10 - onePlace), 10)
+            }
 
             Question(
-                QuestionItem.Plus(value1, value2),
-                answer = Answer.SingleAnswer(value1 + value2)
-            )
+                QuestionItem.Plus(value1, value2), 
+                answer = Answer.SingleAnswer(value1 + value2))
         }
     }
 
